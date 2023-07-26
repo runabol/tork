7e9ecbe3251ba9e7e43037665b284178dd9077b6
Arik Cohen <me@arikcohen.com>
2023-07-26 12:36:02 -0400

    work on the task model

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/tork/manager"
+	"github.com/tork/node"
+	"github.com/tork/task"
+	"github.com/tork/worker"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	t := task.Task{
+		ID:     uuid.New(),
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
+
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+
+	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
+
+	w := worker.Worker{
+		Queue: *queue.New(),
+		DB:    make(map[uuid.UUID]task.Task),
+	}
+	fmt.Printf("worker: %v\n", w)
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDB:  make(map[string][]task.Task),
+		EventDB: make(map[string][]task.TaskEvent),
+		Workers: []string{w.Name},
+	}
+
+	fmt.Printf("manager: %v\n", m)
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		IP:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("node: %v\n", n)
 }
--- task/task.go
+++ task/task.go
@@ -29,6 +29,20 @@ type Task struct {
 	RestartPolicy string
 	StartTime     time.Time
 	FinishTime    time.Time
+	Config        Config
+}
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
 }
 
 type TaskEvent struct {
