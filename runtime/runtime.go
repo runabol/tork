82db6f9fc562e4c0153c54f17f26ae2090793156
Arik Cohen <me@arikcohen.com>
2023-07-26 21:11:24 -0400

    better documentation

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -3,6 +3,6 @@ package runtime
 import "github.com/tork/task"
 
 type Runtime interface {
-	Start(t task.Task) (string, error)
+	Start(t task.Task) (containerID string, err error)
 	Stop(containerID string) error
 }
