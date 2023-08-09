0874578839b0d410fc0a0ff70a9dc7c2075d544e
Arik Cohen <me@arikcohen.com>
2023-08-09 15:32:49 -0400

    Removed unused properties from Task (for now)

--- task/task.go
+++ task/task.go
@@ -20,26 +20,23 @@ const (
 
 // Task is the basic unit of work that a Worker can handle.
 type Task struct {
-	ID            string            `json:"id,omitempty"`
-	Name          string            `json:"name,omitempty"`
-	State         State             `json:"state,omitempty"`
-	CreatedAt     *time.Time        `json:"createdAt,omitempty"`
-	ScheduledAt   *time.Time        `json:"scheduledAt,omitempty"`
-	StartedAt     *time.Time        `json:"startedAt,omitempty"`
-	CompletedAt   *time.Time        `json:"completedAt,omitempty"`
-	FailedAt      *time.Time        `json:"failedAt,omitempty"`
-	CMD           []string          `json:"cmd,omitempty"`
-	Entrypoint    []string          `json:"entrypoint,omitempty"`
-	Run           string            `json:"run,omitempty"`
-	Image         string            `json:"image,omitempty"`
-	Memory        int64             `json:"memory,omitempty"`
-	Disk          int64             `json:"disk,omitempty"`
-	Env           map[string]string `json:"env,omitempty"`
-	RestartPolicy string            `json:"restartPolicy,omitempty"`
-	Queue         string            `json:"queue,omitempty"`
-	Result        string            `json:"result,omitempty"`
-	Error         string            `json:"error,omitempty"`
-	Pre           []Task            `json:"pre,omitempty"`
-	Post          []Task            `json:"post,omitempty"`
-	Volumes       []string          `json:"volumes,omitempty"`
+	ID          string            `json:"id,omitempty"`
+	Name        string            `json:"name,omitempty"`
+	State       State             `json:"state,omitempty"`
+	CreatedAt   *time.Time        `json:"createdAt,omitempty"`
+	ScheduledAt *time.Time        `json:"scheduledAt,omitempty"`
+	StartedAt   *time.Time        `json:"startedAt,omitempty"`
+	CompletedAt *time.Time        `json:"completedAt,omitempty"`
+	FailedAt    *time.Time        `json:"failedAt,omitempty"`
+	CMD         []string          `json:"cmd,omitempty"`
+	Entrypoint  []string          `json:"entrypoint,omitempty"`
+	Run         string            `json:"run,omitempty"`
+	Image       string            `json:"image,omitempty"`
+	Env         map[string]string `json:"env,omitempty"`
+	Queue       string            `json:"queue,omitempty"`
+	Result      string            `json:"result,omitempty"`
+	Error       string            `json:"error,omitempty"`
+	Pre         []Task            `json:"pre,omitempty"`
+	Post        []Task            `json:"post,omitempty"`
+	Volumes     []string          `json:"volumes,omitempty"`
 }
