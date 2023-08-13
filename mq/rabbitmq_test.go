6bd54fdad64983a554b9657a3bd2a61b5e6b0a33
Arik Cohen <me@arikcohen.com>
2023-08-13 16:02:37 -0400

    Added test for RabbitMQ broker

---
+++ mq/rabbitmq_test.go
@@ -0,0 +1,82 @@
+package mq_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tork/job"
+	"github.com/tork/mq"
+	"github.com/tork/node"
+	"github.com/tork/task"
+	"github.com/tork/uuid"
+)
+
+func TestRabbitMQPublishAndSubsribeForTask(t *testing.T) {
+	ctx := context.Background()
+	b, err := mq.NewRabbitMQBroker("amqp://guest:guest@localhost:5672/")
+	assert.NoError(t, err)
+	processed := 0
+	qname := fmt.Sprintf("%stest-%s", mq.QUEUE_EXCLUSIVE_PREFIX, uuid.NewUUID())
+	b.SubscribeForTasks(qname, func(ctx context.Context, t task.Task) error {
+		processed = processed + 1
+		return nil
+	})
+	err = b.PublishTask(ctx, qname, task.Task{})
+	// wait for task to be processed
+	time.Sleep(time.Millisecond * 100)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, processed)
+}
+
+func TestRabbitMQGetQueues(t *testing.T) {
+	ctx := context.Background()
+	b, err := mq.NewRabbitMQBroker("amqp://guest:guest@localhost:5672/")
+	assert.NoError(t, err)
+	qname := fmt.Sprintf("%stest-%s", mq.QUEUE_EXCLUSIVE_PREFIX, uuid.NewUUID())
+	err = b.PublishTask(ctx, qname, task.Task{})
+	assert.NoError(t, err)
+	qis, err := b.Queues(ctx)
+	assert.NoError(t, err)
+	found := false
+	for _, qi := range qis {
+		if qi.Name == qname {
+			found = true
+		}
+	}
+	assert.True(t, found)
+}
+
+func TestRabbitMQPublishAndSubsribeForHeartbeat(t *testing.T) {
+	ctx := context.Background()
+	b, err := mq.NewRabbitMQBroker("amqp://guest:guest@localhost:5672/")
+	assert.NoError(t, err)
+	processed := 0
+	b.SubscribeForHeartbeats(func(ctx context.Context, n node.Node) error {
+		processed = processed + 1
+		return nil
+	})
+	err = b.PublishHeartbeat(ctx, node.Node{})
+	// wait for heartbeat to be processed
+	time.Sleep(time.Millisecond * 100)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, processed)
+}
+
+func TestRabbitMQPublishAndSubsribeForJob(t *testing.T) {
+	ctx := context.Background()
+	b, err := mq.NewRabbitMQBroker("amqp://guest:guest@localhost:5672/")
+	assert.NoError(t, err)
+	processed := 0
+	b.SubscribeForJobs(func(ctx context.Context, j job.Job) error {
+		processed = processed + 1
+		return nil
+	})
+	err = b.PublishJob(ctx, job.Job{})
+	// wait for heartbeat to be processed
+	time.Sleep(time.Millisecond * 100)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, processed)
+}
