b30d8beb2722054f3c2273d122056b7e573745f4
Arik Cohen <me@arikcohen.com>
2023-09-12 15:57:59 -0400

    conf unmarshall support

--- engine/engine.go
+++ engine/engine.go
@@ -29,12 +29,6 @@ const (
 	ModeStandalone  Mode = "standalone"
 )
 
-// OnStartedHandler a bootstrap hook that is
-// called after Tork has finished starting up.
-// If a non-nil error is returned it will
-// terminate the bootstrap process.
-type OnStartedHandler func() error
-
 type Mode string
 
 const (
