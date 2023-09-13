c69e20d71bf7b843812c8da92ebe36ef687d37fe
Arik Cohen <me@arikcohen.com>
2023-09-12 23:13:05 -0400

    logger middleware

--- engine/coordinator.go
+++ engine/coordinator.go
@@ -2,14 +2,18 @@ package engine
 
 import (
 	"crypto/subtle"
+	"fmt"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/runabol/tork/conf"
 	"github.com/runabol/tork/internal/coordinator"
 	"github.com/runabol/tork/internal/uuid"
+	"github.com/runabol/tork/internal/wildcard"
 	"github.com/runabol/tork/middleware/job"
 	"golang.org/x/time/rate"
 )
@@ -75,6 +79,11 @@ func echoMiddleware() []echo.MiddlewareFunc {
 		mw = append(mw, rateLimit(rps))
 	}
 
+	loggerEnabled := conf.BoolDefault("middleware.web.logger.enabled", true)
+	if loggerEnabled {
+		mw = append(mw, logger())
+	}
+
 	return mw
 }
 
@@ -93,6 +102,42 @@ func basicAuth(username, password string) echo.MiddlewareFunc {
 			return true, nil
 		}
 		return false, nil
+	})
+}
+
+func logger() echo.MiddlewareFunc {
+	levelStr := conf.StringDefault("middleware.web.logger.level", "DEBUG")
+	level, err := zerolog.ParseLevel(strings.ToLower(levelStr))
+	if err != nil {
+		panic(err)
+	}
+	skip := conf.StringsDefault("middleware.web.logger.skip", []string{"GET /health"})
+	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:      true,
+		LogStatus:   true,
+		LogRemoteIP: true,
+		LogMethod:   true,
+		Skipper: func(c echo.Context) bool {
+			if len(skip) == 0 {
+				return false
+			}
+			for _, pattern := range skip {
+				if wildcard.Match(pattern, fmt.Sprintf("%s %s", c.Request().Method, c.Request().URL.Path)) {
+					return true
+				}
+			}
+			return false
+		},
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			fmt.Println("here")
+			log.WithLevel(level).
+				Str("URI", v.URI).
+				Str("method", v.Method).
+				Str("remote-ip", v.RemoteIP).
+				Int("status", v.Status).
+				Msg("Request")
+			return nil
+		},
 	})
 }
 
