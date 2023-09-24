614457f0f7e1aaeefd2887d6fc4d8c22e0b3b523
Arik Cohen <me@arikcohen.com>
2023-09-24 14:11:12 -0400

    Cleaned up redundant APIs on CLI

--- cli/cli.go
+++ cli/cli.go
@@ -3,12 +3,7 @@ package cli
 import (
 	"os"
 
-	"github.com/runabol/tork/engine"
 	"github.com/runabol/tork/internal/logging"
-	"github.com/runabol/tork/middleware/job"
-	"github.com/runabol/tork/middleware/node"
-	"github.com/runabol/tork/middleware/task"
-	"github.com/runabol/tork/middleware/web"
 	ucli "github.com/urfave/cli/v2"
 )
 
@@ -27,26 +22,6 @@ func New() *CLI {
 	app.Before = c.before
 	app.Commands = c.commands()
 	return c
-}
-
-func (c *CLI) RegisterWebMiddleware(mw web.MiddlewareFunc) {
-	engine.RegisterWebMiddleware(mw)
-}
-
-func (c *CLI) RegisterTaskMiddleware(mw task.MiddlewareFunc) {
-	engine.RegisterTaskMiddleware(mw)
-}
-
-func (c *CLI) RegisterJobMiddleware(mw job.MiddlewareFunc) {
-	engine.RegisterJobMiddleware(mw)
-}
-
-func (c *CLI) RegisterNodeMiddleware(mw node.MiddlewareFunc) {
-	engine.RegisterNodeMiddleware(mw)
-}
-
-func (c *CLI) RegisterEndpoint(method, path string, handler web.HandlerFunc) {
-	engine.RegisterEndpoint(method, path, handler)
 }
 
 func (c *CLI) Run() error {
