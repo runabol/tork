282cf35fa322903a981182ce33752ef16c556f73
Arik Cohen <me@arikcohen.com>
2023-10-17 13:59:02 -0400

    Host Env middleware

---
+++ middleware/task/hostenv.go
@@ -0,0 +1,43 @@
+package task
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/runabol/tork"
+)
+
+type HostEnv struct {
+	vars map[string]string
+}
+
+func NewHostEnv(vars ...string) (*HostEnv, error) {
+	varsMap := make(map[string]string, 0)
+	for _, varSpec := range vars {
+		parsed := strings.Split(varSpec, ":")
+		if len(parsed) == 1 {
+			varsMap[varSpec] = varSpec
+		} else if len(parsed) == 2 {
+			varsMap[parsed[0]] = parsed[1]
+		} else {
+			return nil, errors.Errorf("invalid env var spec: %s", varSpec)
+		}
+	}
+	return &HostEnv{vars: varsMap}, nil
+}
+
+func (m *HostEnv) Execute(next HandlerFunc) HandlerFunc {
+	return func(ctx context.Context, et EventType, t *tork.Task) error {
+		if et == StateChange && t.State == tork.TaskStateScheduled {
+			if t.Env == nil {
+				t.Env = make(map[string]string)
+			}
+			for name, alias := range m.vars {
+				t.Env[alias] = os.Getenv(name)
+			}
+		}
+		return next(ctx, et, t)
+	}
+}
