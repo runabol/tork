ea831e11bcdfad8287e4500bfc1c94e9aa8d3838
Arik Cohen <me@arikcohen.com>
2025-02-18 19:30:35 -0500

    wip

--- runtime/service/service_test.go
+++ runtime/service/service_test.go
@@ -66,6 +66,61 @@ func Test_Run(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+func Test_Run_Concurrent(t *testing.T) {
+	rt, err := docker.NewDockerRuntime()
+	assert.NoError(t, err)
+
+	p := NewServiceRuntime(rt)
+
+	ctx := context.Background()
+	task := &tork.Task{
+		ID:    uuid.NewUUID(),
+		Image: "node:lts-alpine3.20",
+		Run:   "node server.js",
+		Files: map[string]string{
+			"server.js": `
+			 const http = require('http');
+			 const hostname = '0.0.0.0';
+			 const port = 8080;
+			 const server = http.createServer((req, res) => {
+			   res.statusCode = 200;
+			   res.setHeader('Content-Type', 'text/plain');
+			   res.end('Hello World\n');
+			 });
+			server.listen(port, hostname, () => {
+			  console.log('server running');
+			});
+			`,
+		},
+		Service: &tork.Service{
+			Name: "hello-service",
+			Port: "8080",
+			Path: "/test1",
+		},
+	}
+
+	errCh := make(chan error, 2)
+
+	go func() {
+		errCh <- p.Run(ctx, task)
+	}()
+
+	go func() {
+		errCh <- p.Run(ctx, task)
+	}()
+
+	for i := 0; i < 2; i++ {
+		err := <-errCh
+		assert.NoError(t, err)
+	}
+
+	assert.Equal(t, "Hello World\n", task.Result)
+	assert.Equal(t, "Hello World\n", task.Result)
+
+	err = p.Shutdown(ctx)
+	assert.NoError(t, err)
+}
+
 func Test_Run_ServiceFieldRequired(t *testing.T) {
 	rt, err := docker.NewDockerRuntime()
 	assert.NoError(t, err)
