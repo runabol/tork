79a8e489ec9cd7eee5ed20ac5204aec95aa8a1bc
Arik Cohen <me@arikcohen.com>
2026-02-21 11:25:26 -0500

    Fix: connection leaks

--- engine/locker.go
+++ engine/locker.go
@@ -1,6 +1,8 @@
 package engine
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 	"github.com/runabol/tork/conf"
 	"github.com/runabol/tork/locker"
@@ -25,7 +27,12 @@ func (e *Engine) createLocker(ltype string) (locker.Locker, error) {
 			"locker.postgres.dsn",
 			conf.StringDefault("datastore.postgres.dsn", "host=localhost user=tork password=tork dbname=tork port=5432 sslmode=disable"),
 		)
-		return locker.NewPostgresLocker(dsn)
+		return locker.NewPostgresLocker(dsn,
+			locker.WithMaxOpenConns(conf.IntDefault("locker.postgres.max_open_conns", conf.IntDefault("datastore.postgres.max_open_conns", 25))),
+			locker.WithMaxIdleConns(conf.IntDefault("locker.postgres.max_idle_conns", conf.IntDefault("datastore.postgres.max_idle_conns", 25))),
+			locker.WithConnMaxLifetime(conf.DurationDefault("locker.postgres.conn_max_lifetime", conf.DurationDefault("datastore.postgres.conn_max_lifetime", time.Hour))),
+			locker.WithConnMaxIdleTime(conf.DurationDefault("locker.postgres.conn_max_idle_time", conf.DurationDefault("datastore.postgres.conn_max_idle_time", time.Minute*5))),
+		)
 	default:
 		return nil, errors.Errorf("unknown locker type: %s", ltype)
 	}
--- locker/postgres.go
+++ locker/postgres.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/binary"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -12,18 +13,48 @@ import (
 )
 
 type PostgresLocker struct {
-	db *sqlx.DB
+	db              *sqlx.DB
+	maxOpenConns    int
+	maxIdleConns    int
+	connMaxLifetime time.Duration
+	connMaxIdleTime time.Duration
 }
 
 type postgresLock struct {
 	tx *sqlx.Tx
 }
 
+type LockerOption func(*PostgresLocker)
+
+func WithMaxOpenConns(n int) LockerOption {
+	return func(l *PostgresLocker) {
+		l.maxOpenConns = n
+	}
+}
+
+func WithMaxIdleConns(n int) LockerOption {
+	return func(l *PostgresLocker) {
+		l.maxIdleConns = n
+	}
+}
+
+func WithConnMaxLifetime(d time.Duration) LockerOption {
+	return func(l *PostgresLocker) {
+		l.connMaxLifetime = d
+	}
+}
+
+func WithConnMaxIdleTime(d time.Duration) LockerOption {
+	return func(l *PostgresLocker) {
+		l.connMaxIdleTime = d
+	}
+}
+
 func (l *postgresLock) ReleaseLock(ctx context.Context) error {
 	return l.tx.Rollback()
 }
 
-func NewPostgresLocker(dsn string) (*PostgresLocker, error) {
+func NewPostgresLocker(dsn string, opts ...LockerOption) (*PostgresLocker, error) {
 	db, err := sqlx.Connect("postgres", dsn)
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to connect to postgres")
@@ -34,7 +65,24 @@ func NewPostgresLocker(dsn string) (*PostgresLocker, error) {
 	if err := db.Ping(); err != nil {
 		return nil, errors.Wrapf(err, "failed to ping database")
 	}
-	return &PostgresLocker{db: db}, nil
+	locker := &PostgresLocker{db: db}
+	for _, opt := range opts {
+		opt(locker)
+	}
+	// Apply connection pool settings
+	if locker.maxOpenConns > 0 {
+		db.SetMaxOpenConns(locker.maxOpenConns)
+	}
+	if locker.maxIdleConns > 0 {
+		db.SetMaxIdleConns(locker.maxIdleConns)
+	}
+	if locker.connMaxLifetime > 0 {
+		db.SetConnMaxLifetime(locker.connMaxLifetime)
+	}
+	if locker.connMaxIdleTime > 0 {
+		db.SetConnMaxIdleTime(locker.connMaxIdleTime)
+	}
+	return locker, nil
 }
 
 func (p *PostgresLocker) AcquireLock(ctx context.Context, key string) (Lock, error) {
@@ -45,9 +93,11 @@ func (p *PostgresLocker) AcquireLock(ctx context.Context, key string) (Lock, err
 	}
 	var lockAttempt bool
 	if err := tx.GetContext(ctx, &lockAttempt, "SELECT pg_try_advisory_xact_lock($1)", keyHash); err != nil {
+		_ = tx.Rollback()
 		return nil, errors.Wrapf(err, "failed to acquire lock for key '%s'", key)
 	}
 	if !lockAttempt {
+		_ = tx.Rollback()
 		return nil, errors.Errorf("failed to acquire lock for key '%s'", key)
 	}
 	return &postgresLock{tx: tx}, nil
