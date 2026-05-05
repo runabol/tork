59313ac89358e8947ed68b8e5c8b06eeaa0a1420
chip.selden <chip.selden@disney.com>
2026-05-05 12:14:33 -0700

    feat: reject unknown mount opts for bind and tmpfs mounts
    Volume mounts pass opts through to the driver, but bind/tmpfs have a
    fixed set of meaningful options. Reject unknown keys to catch typos and
    prevent users from assuming unsupported options are honored.

--- mount.go
+++ mount.go
@@ -1,12 +1,55 @@
 package tork
 
-import "golang.org/x/exp/maps"
+import (
+	"fmt"
+
+	"golang.org/x/exp/maps"
+)
 
 const (
 	MountTypeVolume string = "volume"
 	MountTypeBind   string = "bind"
 	MountTypeTmpfs  string = "tmpfs"
 )
+
+var allowedPropagation = map[string]bool{
+	"private":  true,
+	"rprivate": true,
+	"slave":    true,
+	"rslave":   true,
+}
+
+var allowedBindOpts = map[string]bool{
+	"propagation": true,
+	"readonly":    true,
+}
+
+var allowedTmpfsOpts = map[string]bool{
+	"readonly": true,
+}
+
+func ValidateMountOpts(m *Mount) error {
+	var allowed map[string]bool
+	switch m.Type {
+	case MountTypeBind:
+		allowed = allowedBindOpts
+	case MountTypeTmpfs:
+		allowed = allowedTmpfsOpts
+	default:
+		return nil
+	}
+	for key := range m.Opts {
+		if !allowed[key] {
+			return fmt.Errorf("unsupported option %q for %s mount", key, m.Type)
+		}
+	}
+	if prop, ok := m.Opts["propagation"]; ok {
+		if !allowedPropagation[prop] {
+			return fmt.Errorf("unsupported mount propagation: %s", prop)
+		}
+	}
+	return nil
+}
 
 type Mount struct {
 	ID     string            `json:"-"`
